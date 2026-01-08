5fd2a2839ed2645fdfb074f36843f7789d34e773
shogetoshi <188074504+shogetoshi@users.noreply.github.com>
2026-01-08 12:07:01 +0900

    ディレクトリ表示に対応

--- source_command.sh
+++ source_command.sh
@@ -5,11 +5,14 @@ COMMAND_HISTORY_FILE="$1"
 if [[ $# -ge 2 ]]; then
     current_realdir="${2}"
     tac "$COMMAND_HISTORY_FILE" | \
-        jq -r --arg realdir "$current_realdir" 'select(.realdir == $realdir) | .command' 2>/dev/null | \
+        jq -r --arg realdir "$current_realdir" 'select(.realdir == $realdir) | "\t\(.command)"' 2>/dev/null | \
         awk '!a[$0]++' | \
-        bat --plain --color always --language bash
+        bat --plain --color always --language bash | \
+        tr '\n' '\0' | sed -z 's/\t/\n/'
 else
     tac "$COMMAND_HISTORY_FILE" | \
-        jq -r '.command' 2>/dev/null | \
-        bat --plain --color always --language bash
+        jq -r '"\(.dir)\t\(.command)"' 2>/dev/null | \
+        awk '!a[$0]++' | \
+        bat --plain --color always --language bash | \
+        tr '\n' '\0' | sed -z 's/\t/\n/'
 fi
